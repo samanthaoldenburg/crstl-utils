5eba9d477bdb4980fe7aa8eb867ffe719217cbbd
Samantha Oldenburg <s.wolfgang.oldenburg@gmail.com>
2026-01-11 10:36:52 -0600

    chore: remove dist

--- dist/scripts/crstl-utils.js
+++
@@ -1,2 +0,0 @@
-Hooks.once("init",()=>{console.log("Hello world!"),o.myTest(),l()});function l(){window.CrstlUtils=o}class o{static myTest=()=>{console.log("Howdy!")}}
-//# sourceMappingURL=crstl-utils.js.map
