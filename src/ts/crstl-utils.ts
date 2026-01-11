c587e3efc0f09ca5053d81bfce7ad1c659f3bcd4
Samantha Oldenburg <s.wolfgang.oldenburg@gmail.com>
2026-01-11 10:04:20 -0600

    feat: un-yarn

--- src/ts/crstl-utils.ts
+++ src/ts/crstl-utils.ts
@@ -1,4 +1,14 @@
 Hooks.once("init", () => {
  console.log("Hello world!");
+  CrstlUtils.myTest();
 });
 
+Hooks.once("Ready", () => {
+  
+});
+
+export class CrstlUtils {
+  static myTest = (): void => {
+    console.log("Howdy!");
+  }
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
+import * as fsPromises from "fs/promises";
 import copy from "rollup-plugin-copy";
-import { defineConfig } from "vite";
+import scss from "rollup-plugin-scss";
+import { defineConfig, Plugin } from "vite";
 
 export default defineConfig({
   build: {
@@ -20,3 +22,29 @@ export default defineConfig({
     }),
   ],
 });
+
+function updateModuleManifestPlugin(): Plugin {
+ return {
+   name: "update-module-manifest",
+   async writeBundle(): Promise<void> {
+     const moduleVersion = process.env.MODULE_VERSION;
+     const manifestContents: string = await fsPromises.readFile(
+       "src/module.json",
+       "utf-8"
+     );
+     const manifestJson = JSON.parse(manifestContents) as Record<
+       string,
+       unknown
+     >;
+
+     if (moduleVersion) {
+       manifestContents["version"] = moduleVersion;
+     }
+
+     await fsPromises.writeFile(
+       "dist/module.json",
+       JSON.stringify(manifestJson, null, 4)
+     );
+   },
+ };
+}
