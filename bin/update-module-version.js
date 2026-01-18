f738e7ff015aa9fcbf9daf4720d022c6f07aafd1
Samantha Oldenburg <s.wolfgang.oldenburg@gmail.com>
2026-01-18 12:54:08 -0600

    chore: make publish helper

---
+++ bin/update-module-version.js
@@ -0,0 +1,28 @@
+// Update foundry module
+//
+
+const execSync = require('child_process').execSync;
+const fs = require('fs')
+const path = require('path')
+
+const FOUNDRY_MODULE = path.join(__dirname, '../src/module.json')
+const PACKAGE_FILE = path.join(__dirname, '../package.json')
+
+const runCommand = (command) => {
+  console.log("Running command: ", command)
+  return execSync(command)
+}
+
+runCommand(`npm version ${process.argv.slice(2).join(' ')}`)
+
+const foundryInfo = JSON.parse(fs.readFileSync(FOUNDRY_MODULE))
+const packageInfo = JSON.parse(fs.readFileSync(PACKAGE_FILE))
+
+newVersion = packageInfo.version
+foundryInfo.version = newVersion
+
+fs.writeFileSync(FOUNDRY_MODULE, JSON.stringify((foundryInfo, null, 2)))
+
+runCommand(`git add ${FOUNDRY_MODULE}`)
+runCommand(`git commit --amend --no-edit`)
+runCommand(`git tag -fa ${process.argv.slice(3).join(' ')} v${newVersion}`)
