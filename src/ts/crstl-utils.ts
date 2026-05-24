c54f63d1ae9e09f8cb738258e5a7beee1279e4de
Samantha Oldenburg <s.wolfgang.oldenburg@gmail.com>
2026-05-24 11:10:39 -0500

    feat: add way to delegate macros to GM

--- src/ts/crstl-utils.ts
+++ src/ts/crstl-utils.ts
@@ -38,7 +38,40 @@ export class CrstlUtils {
     this.sceneFactory = new CFSceneFactory(this.game)
   }
 
+  public async executeMacroAsGm(macroName: string, macroArguments: unknown): Promise<void> {
+    if (!game.macros) return;
+
+    const macro = game.macros.getName(macroName);
+
+    if (!macro) {
+      return this.uiError(`Macro ${macroName} does not exist`);
+    }
+
+    // Use the Advanced Macro's query engine directly
+    // This may need to be updated when Foundry updates
+    const gmUser = game.users.activeGM;
+
+    if (!gmUser) {
+      return this.uiWarn("GM isn't logged in. Cannot execute command.");
+    }
+
+    const queryData = { macro: macro.id, scope: macroArguments };
+
+    // It's very hard to type coerce this due to the typing around CONFIG.queries
+    //
+    // @ts-expect-error
+    gmUser.query("advanced-macros.executeMacro", queryData, {timeout: 30000})
+  }
+
   public currentScene(): CFScene {
     return this.sceneFactory.buildFromScene(this.game.scenes.current as Scene);
+  }
+
+  private uiWarn(message: string) {
+    ui?.notifications?.warn(message);
+  }
+
+  private uiError(message: string) {
+    ui?.notifications?.error(message);
   }
 }
